84ac71d2743b1facc9aab8a4e087e730ca8800c8
Armando <roblesrioarmand@gmail.com>
2017-11-21 19:09:36 -0600

    underscores for word and get key values

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,7 @@
-document.onkeyup = function() {
+$(function(){
+
+
+//document.onkeyup = function() {
 		//Definition of the array of cities of USA
 		var capitals = [
 			"Albany", "Austin", "Helena",  "Juneau",
@@ -6,6 +9,7 @@ document.onkeyup = function() {
 		];
 		var city="";
 		console.log(city);
+	
 	//Randomly selection of cities in array of capitals
 
 		//Random number for the length of the array 
@@ -13,25 +17,22 @@ document.onkeyup = function() {
 		console.log("The city inthe array is the #: " + i);
 		
 		function guessCity() {
-//			document.write("Guess the US city: " + capitals[i]);
-			var city = capitals[i];
+			city = capitals[i];
 			for ( j = 0; j < city.length; j++) {
 				//Here assign every letter of the city to a variable
 				var newvar = city.charAt(j);
 				var onScreen = $("<h1>");
 				onScreen.text(newvar);
-//			   	$("#demo").append(onScreen);
 			   	$("#demo").append(" _ ");
-			   	//Here, write an underline _ for every letter of the city.
-//				document.write(" _ ");
+			   	console.log("text");
 			}
-			console.log(city);
-			myFunction();
+			console.log("the city in guessCity: " + city);
+//			myFunction();
 		}
 
 		//reading the city from the array one letter at the time
 		function myFunction(char) {
-			for (userChar !=== "") {
+//			for (userChar !=== "") {
 				console.log (city);
 			   	for (var l = 0; l < city.length; l++ ) {
 			   		var newvar = city.charAt(l);
@@ -41,26 +42,23 @@ document.onkeyup = function() {
 			   		$("#demo").append(newvar);
 			   		console.log (newvar);
 				}
-			}
-		}
+//			}
+		} // End myFunct
 
 //			document.write("(La palabra " + city + " tiene " + j + " letras.)");
 //			console.log(city);
 //			console.log(city.length);
 guessCity();
+console.log("city at the end: " + city);
+//}
 
-}
+/*
+var   arr = [];
+document.onkeyup = function(event) {
+  var letter = event.key;
+    arr.push(letter)
+    console.log(arr)
+};
+*/
 
- $("#demo").on("keyup change", function() {
- 	userChar = this.userChar;
- 	myFunction(userChar)
-}
-/**      // Let's start by grabbing a reference to the <span> below.
-      var userText = document.getElementById("#demo");
-
-      // Next, we give JavaScript a function to execute when onkeyup event fires.
-      document.onkeyup = function(event) {
-        userText.textContent = event.key;
-//        console.log("userText =" + userText.textContent );         
-      };**/
-
+}); // end ready

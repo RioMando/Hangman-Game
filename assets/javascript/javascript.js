8bfa3e38e5b4d08226e48eefd2eebb0ee428b14d
Armando <roblesrioarmand@gmail.com>
2017-11-24 22:38:14 -0600

    functional code, basic mode

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,14 @@
 $(function(){
 
   var capitals = [
-    "albany", "austin", "helena", "juneau",
-    "lincoln", "phoenix", "sacramento", "trenton"
+    "albany",
+    "austin",
+    "helena",
+    "juneau",
+    "lincoln",
+    "phoenix",
+    "sacramento",
+    "trenton"
   ];
   var city, newCity="";
   var ciudad = "";
@@ -10,10 +16,12 @@ $(function(){
   var i;
   var cityToGuess = [];
   var countWin = 0;
+  var count = 0;
   var gessLft = 9;
   //Randomly selection of cities in array of capitals
 
   function updateScore() {
+  	$("#wins").empty();
     var newWin = countWin.toString();
     var newWin = $("<h3>");
     $("#wins").append(" " + countWin);
@@ -21,6 +29,7 @@ $(function(){
   }
 
   function updateGuess(){
+    $("#guessLft").empty();
     var nwGss = gessLft.toString();
     var nwGss = $("<h3>");
     $("#guessLft").append(" " + gessLft);
@@ -30,39 +39,71 @@ $(function(){
     i = Math.floor(Math.random()*capitals.length);
     city = capitals[i];
     console.log("The city inthe array is the # " + i + ": " + city);
-  	console.log("city in initCity: " + city);
- 	for (j = 0; j < city.length; j++) {
+  	for (j = 0; j < city.length; j++) {
  		cityToGuess[j] = "_";
- 	}
+ 	}//End for-loop
  	console.log("City to guess @ initCity: " + cityToGuess);
   }//End initCity()
 
   function displayCity() {
    	for (var j = 0; j < city.length; j++) {
   	  if (j == 0) {
-  	  	cityToGuess[j] = cityToGuess[j].toUpperCase();
-  	  //console.log("city here@ display city: " + otro);
+  	  	cityToGuess[j] = cityToGuess[j].toUpperCase();//To assure capitalize cities
       }//End if-else
       $("#citGss").append(cityToGuess[j] + " ");
     }//End for-loop
   }//End displayCity()
 
+/*  function displayPic() {
+    $("citGss").empty();
+    $("#citGss").append("src=assets/images/austin.jpg");	
+  }*/
+
   function guessLetter() {
+    var z=0;
     for (var j = 0; j < city.length; j++) {
       var newvar = city.charAt(j); //Assign every letter of the city to newvar
       if (cityToGuess[j] === "_") {
         if (letter == newvar) {
         	cityToGuess[j] = letter;
+        	count++;
+            z=1;
         }//End 1st if-else loop
       }//End 2nd if-else loop
-    }//End for-loop 
+    }//End for-loop
+    if (z===0){
+      var lett = $("<h3>");
+      console.log("letter inside display: " + letter);
+      $("#yourG").append(letter);
+      gessLft--;
+      updateGuess();
+    }//End if-loop
+    
+    function resetSettings(){
+      $("#yourG").empty();
+      gessLft=9;
+      updateScore();
+      count = 0;
+      initCity();
+      updateGuess();
+    }
+    if (count===city.length) {
+      countWin++;
+      resetSettings()
+    }
+    if (gessLft===0) {
+    	resetSettings();
+    }
+
     $("#citGss").empty();
     displayCity();
+    if (gessLft===0){
+
+    }
   }//End guessLetter() 
 
   document.onkeyup = function(event) {
     letter = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("Not good");
     guessLetter();
   };//End onkeyup
 
